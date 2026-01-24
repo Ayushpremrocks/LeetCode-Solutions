1a6c8b0de06655d32fdb07528851e62803c66a28
Ayush Prem <ayushpremrocks@gmail.com>
2026-01-24 14:30:44 +0530

    Time: 190 ms (5.47%) | Memory: 100.1 MB (30.18%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -4,9 +4,7 @@ public:
         int mini=0;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n/2;i++){
-            mini = max(mini,nums[i]+nums[n-i-1]);
-        }
+        for(int i=0;i<n/2;i++) mini = max(mini,nums[i]+nums[n-i-1]);
         return mini;
     }
 };
